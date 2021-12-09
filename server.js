7b2f93974131acaff899ac180f2cafa555b215ad
tin <tin.caotrong@gmail.com>
2021-12-09 17:45:41 +0700

    update

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const token = process.env.BOT_TOKEN;
 const baseurl = process.env.BASE_URL;
 const bot = new TelegramBot(token, {polling: true});
 const emailedHeroes = []
+const listContacts = []
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  listContacts.push(chatId)
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(793210862, 'Bot received your message');
+  bot.sendMessage(chatId, 'Bot received your message');
 });
 
 
@@ -23,6 +25,9 @@ async function findCheapestHero() {
     const currentBnbPrice = Number(currentBnbPriceObj.price);
     heroes = await Promise.all(
       heroes.map(async (hero) => {
+        let heroProfile = await axios.get(`https://data.thetanarena.com/thetan/v1/hero?id=${hero.refId}`)
+        heroProfile = heroProfile.data.data;
+        hero.heroProfile = heroProfile
         const heroPriceBybnbPrice = Number(`0.${hero.price}`);
         const heroPriceByUSD = heroPriceBybnbPrice * currentBnbPrice;
         hero.usdPrice = parseFloat(heroPriceByUSD).toFixed(2);
@@ -31,9 +36,13 @@ async function findCheapestHero() {
     );
     for (let i = 0; i < heroes.length; i++) {
       const currentHero = heroes[i];
-      if (currentHero.usdPrice < 100 && currentHero.battleCap < 100 && !emailedHeroes.includes(currentHero.refId)) {
+      if (Number(currentHero.usdPrice) < 80 && Number(currentHero.heroProfile.heroRanking.battleCapTHC) < 70 && !emailedHeroes.includes(currentHero.refId)) {
         emailedHeroes.push(currentHero.refId)
-        bot.sendMessage(793210862, `Giá: ${currentHero.usdPrice}, Đã chơi: ${currentHero.battleCap} trận, Link mua: https://marketplace.thetanarena.com/item/${currentHero.refId}`);
+        for (let j = 0; j < listContacts.length; j++) {
+          const contact = listContacts[j];
+          const message = `Tên tướng: ${currentHero.heroProfile.heroInfo.name},\n Giá: ${currentHero.usdPrice},\n Đã chơi: ${currentHero.heroProfile.heroRanking.battleCapTHC}/${currentHero.heroProfile.heroRanking.totalBattleCapTHC} trận,\n Link mua: https://marketplace.thetanarena.com/item/${currentHero.refId}`
+          bot.sendMessage(contact, message);
+        }
       }
     }
   } catch (error) {
